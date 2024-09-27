6d16f67d9b62a6742b1355827f19ca2ea08a3dab
Muhammad Maraj Khan <mairajkhan11345@gmail.com>
2024-09-27 18:54:35 +0500

    feat: social links

--- src/constants/index.js
+++ src/constants/index.js
@@ -171,22 +171,17 @@ export const socialMedia = [
   {
     id: "social-media-1",
     icon: instagram,
-    link: "https://www.instagram.com/",
+    link: "https://www.instagram.com/primech33?igsh=cDA3c2xqcTlpdHNl",
   },
   {
     id: "social-media-2",
     icon: facebook,
-    link: "https://www.facebook.com/",
+    link: "https://www.facebook.com/people/Primech/100088849702145/",
   },
   {
     id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
-  },
-  {
-    id: "social-media-4",
     icon: linkedin,
-    link: "https://www.linkedin.com/",
+    link: "https://pk.linkedin.com/company/primech-agency",
   },
 ];
 
